5d2d7b4b9c7857e5c6c73e96c1e6a008f68ccdfe
jonathandelprado <jonathan.delprado@wilsongroupau.com>
2024-07-25 21:56:35 +0800

    done all input calculations - still inaccurate

--- js/custom.js
+++ js/custom.js
@@ -87,8 +87,10 @@ $(document).ready(function(){
                 break;
         }
 
-        let od = com = pc = sc = barc = spbcc = bd = br = bci = rc = pparv = lf = pmd = 0;
-
+        let od = com = pc = sc = barc = spbcc = bd = br = bci = rc = pparv = lf = pmd = ppsf = np = tnp = ppc =
+            pp = ppr = pr = arv80 = bp = sfc = tle = ba = bcc = thc = allc = np80bp = lbearcltv = lbearncltv = lbearc =
+            lbearnc = cop = arv70 = 0;
+        
         if (sales_price_resale != 0) {
 
             if (cost_to_buy != 0) {
@@ -120,34 +122,57 @@ $(document).ready(function(){
                 }
 
                 //PURCHASE COST - INACCURATE
-                if (cost_to_buy != 0) {
-                    pc = cost_to_buy *(-0.0055)+400;
-                    $(".pc").html("$"+pc);
-                }
+                pc = cost_to_buy *(-0.0055)+400;
+                $(".pc").html("$"+pc);
+                $(".purpri").html("$"+pc);
 
-                //Purchase PRICE % of ARV & LOAN Amount % of ARV
+                //Purchase PRICE % of ARV & LOAN Amount % of ARV & Detailed Flip % of PP to ARV
                 pparv = (cost_to_buy/sales_price_resale ) * 100;
                 $(".pparv").html(pparv.toFixed(2)+"%");
                 $(".laarv").html(pparv.toFixed(2)+"%");
+                $(".dfarv").html(pparv.toFixed(2)+"%");
+
             }
 
             //SALES COST - INACCURATE
             sc = sales_price_resale * -(0.0055)+400;
             $(".sc").html("$"+sc);
 
-            // BUYER'S AGENT REALTOR COMMISSION
+            // BUYER'S AGENT REALTOR COMMISSION /   Buyer's Agent
             barc = sales_price_resale * commission_sale;
             $(".barc").html("$"+barc);
+            $(".ba").html("$"+barc);
 
-            //SELLER PAID BUYER'S CLOSING COSTS
+            //SELLER PAID BUYER'S CLOSING COSTS /   Buyer's CC
             spbcc = (commission_sale / 100) * sales_price_resale;
             $(".spbcc").html("$"+spbcc);
+            $(".bcc").html("$"+spbcc);
+
+
+            //Price Per SF, ARV
+            if (home_sf != 0) {
+                ppsf = sales_price_resale / home_sf;
+                $(".ppsf").html("$"+ppsf.toFixed(2));
+            }
+
+            // ARV
+            $(".arv").html("$"+sales_price_resale);
+
+            // 80% of ARV
+            arv80 = sales_price_resale * 0.8;
+            $(".80arv").html("$"+arv80);
+
+            //BUY PRICE / 80% Rule = this PP
+            bp = arv80 + rc;
+            $(".bp").html("$"+bp);
+            $(".80pp").html("$"+bp);
 
         }
 
         //BORROWER DOWNPAYMENT
         bd = (down_payment / 100) * cost_to_buy;
         $(".bd").html("$"+bd);
+        $(".dp").html("$"+bd);
 
         //BORROWER CASH IN
         bci = rc + bd;
@@ -155,13 +180,97 @@ $(document).ready(function(){
 
         //REHAB COSTS & BORROWER REPAIR - INACCURATE
         if (home_sf != 0) {
-            let input_cost_val = $("[name='level-repair']").find(":selected").attr("cost-value");
+
+            let input_cost_val = 0;
+            let repair_selected = $('[name="level-repair"]').find(":selected").text();
+
+            switch (repair_selected) {
+                case "Basic":
+                    input_cost_val = 35;
+                    break;
+                case "Medium":
+                    input_cost_val = 50;
+                    break;
+                case "Everything":
+                    input_cost_val = 80;
+                    break;
+                case "Other":
+                    input_cost_val = $("[name='bid']").val();
+                    break;
+            }
+
+            console.log(repair_selected);
+
             if (input_cost_val != null) {
-                var rc = Math.abs(-1 * home_sf * parseInt(input_cost_val));
+                var rc = (repair_selected != "Other") ? Math.abs(-1 * home_sf * parseInt(input_cost_val)) : parseInt(input_cost_val);
                 $(".rc").html("$"+rc);
                 $(".br").html("$"+rc);
+                $(".rehab").html("$"+rc);
+                $(".rmc").html("$"+rc);
             }
         }
+
+        //NET PROFIT
+        np = od + com + sc + barc + spbcc + rc;
+        $(".np").html("$"+np);
+
+        //TOTAL NET PROFIT
+        tnp = np + ppc;
+        $(".tnp").html("$"+tnp);
+
+        //Profit as % of P.P.
+        pp = np / cost_to_buy;
+        $(".pp").html(pp.toFixed(2)+"%");
+
+        //Profit as % of P.P. & Rehab
+        ppr = np / (cost_to_buy + np);
+        console.log(ppr.toFixed(2));
+        $(".ppr").html(ppr.toFixed(2)+"%");
+
+        //Profit as % of Rehab
+        pr = np / -rc;
+        $(".pr").html(pr.toFixed(2)+"%");
+
+        //TOTAL HARD COST
+        thc = cost_to_buy + rc;
+        $(".thc").html("$"+thc);
+
+        //SOFT COSTS
+        sfc = com + spbcc + tle + barc;
+        $(".sfc").html("$"+sfc);
+
+        //TITLE
+        tle = pc + sc;
+        $(".tle").html("$"+tle);
+
+        //ALL COSTS
+        allc = thc + sfc;
+        $(".allc").html("$"+allc);
+
+        //Net Profit based on inputs above but with 80% rule "Buy Price" - INACCURATE
+        let np80bp_sum = com + pc + sc + barc + spbcc;
+        np80bp = ((sales_price_resale - bp ) + np80bp_sum + rc);
+        $(".np80bp").html("$"+np80bp);
+
+        //Lender Break Even - Assuming Rehab is Complete
+        lbearc = lf + np80bp_sum;
+        lbearcltv = lbearc / sales_price_resale;
+        $(".lbearc").html("$"+lbearc);
+        $(".lbearcltv").html(lbearcltv.toFixed(2)+"%");
+
+        //Lender Break Even - Assuming Rehab is NOT Complete
+        lbearnc= lbearcltv + rc;
+        lbearncltv = lbearnc + rc;
+        $(".lbearnc").html("$"+lbearnc);
+        $(".lbearncltv").html(lbearncltv.toFixed(2)+"%");
+
+        //Cash out of pocket
+        cop = bd + rc;
+        $(".cop").html("$"+cop);
+
+        //70% of ARV Rule = this PP
+        arv70 = sales_price_resale * 0.7;
+        $(".70arvpp").html("$"+arv70);
 
     });
 
